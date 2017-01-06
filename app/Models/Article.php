5f30d06b04d2a186b669fa26911c1b3e5900bcd4
SadCreeper <87826632@qq.com>
2017-01-06 12:28:36 +0800

    finish article model creation

---
+++ app/Models/Article.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    //数据表名称
+    protected $table = 'articles';
+}
---
+++ database/migrations/2017_01_06_031257_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('intro')->nullable();
+            $table->string('content')->nullable();
+            $table->integer('view')->default(0);
+            $table->integer('comment')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
