67fc0fed6bb48ded4d0ba64f56c8837c05d96ef3
SadCreeper <87826632@qq.com>
2017-01-10 09:52:52 +0800

    finish article view function

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -17,6 +17,12 @@ class ArticleController extends Controller
         //markdown 解析
         $article->content = Markdown::convertToHtml($article->content);
 
+        //更新浏览量
+        $article->view = $article->view + 1;
+        $article->update([
+            'view' => $article->view,
+        ]);
+
         return view('article.show', compact('article'));
     }
 }
