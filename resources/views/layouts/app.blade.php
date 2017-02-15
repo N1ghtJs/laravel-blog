e9801ca6ebf00776186476886080a0da58d2f1af
SadCreeper <87826632@qq.com>
2017-02-15 15:16:26 +0800

    add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,10 @@
 
     <title>@yield('title', '无标题') - lhz的个人主页</title>
 
+    <!-- icon -->
+    <link rel="shortcut icon" href="/img/favicon.ico" />
+    <link rel="bookmark" href="/img/favicon.ico" type="image/x-icon"　/>
+
     <!-- Styles -->
     <link href="/css/app.css" rel="stylesheet">
 
