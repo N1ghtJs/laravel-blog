cc74d358306fa2681be6152e5fcdf2bd2939e14b
SadCreeper <87826632@qq.com>
2017-02-16 13:21:56 +0800

    locale carbon

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Carbon\Carbon;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -13,7 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Carbon 本地化 中文
+        Carbon::setLocale('zh');
     }
 
     /**
