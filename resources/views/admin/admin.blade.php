55b556473d7c3ee0e5b38114d9c4c434e079061a
SadCreeper <87826632@qq.com>
2017-03-22 11:56:03 +0800

    fix bug of javascript disabled in admin pages

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -21,47 +21,29 @@
     <!-- Scripts -->
 </head>
 <body>
-    {{--电脑版 管理后台--}}
-    <div class="hidden-xs">
         {{--左侧垂直菜单栏--}}
         <div class="z-admin-menu">
             <div class="header">
-                <a href="{{ route('admin') }}">管理后台</a>
+                <!-- 电脑显示(>768px) -->
+                <div class="hidden-xs">
+                    <a href="{{ route('admin') }}">管理后台</a>
+                </div>
+                <!-- 手机显示(<768px) -->
+                <div class="visible-xs-block">
+                    <a href="{{ route('admin') }}"><span class="glyphicon glyphicon-cog"></span></a>
+                </div>
             </div>
             <ul CLASS="list">
-
-                <li><a href="{{ route('home') }}"><span class="glyphicon glyphicon-cog"></span>返回首页</a></li>
-
-                <li><a href="{{ route('article.index') }}"><span class="glyphicon glyphicon-cog"></span>文章管理</a></li>
-
-
-                <!--
-                <li><a href="#collapse2" data-toggle="collapse"><span class="glyphicon glyphicon-cog"></span>菜单3</a></li>
-                <ul id="collapse2" class="collapse">
-                    <li><a href="">菜单31</a></li>
-                    <li><a href="">菜单32</a></li>
-                    <li><a href="">菜单33</a></li>
-                    <li><a href="">菜单31</a></li>
-                    <li><a href="">菜单32</a></li>
-                    <li><a href="">菜单33</a></li>
-                    <li><a href="">菜单31</a></li>
-                    <li><a href="">菜单32</a></li>
-                    <li><a href="">菜单33</a></li>
-                    <li><a href="">菜单31</a></li>
-                    <li><a href="">菜单32</a></li>
-                    <li><a href="">菜单33</a></li>
-                    <li><a href="">菜单31</a></li>
-                    <li><a href="">菜单32</a></li>
-                    <li><a href="">菜单33</a></li>
-                    <li><a href="">菜单31</a></li>
-                    <li><a href="">菜单32</a></li>
-                    <li><a href="">菜单33</a></li>
-                    <li><a href="">菜单31</a></li>
-                    <li><a href="">菜单32</a></li>
-                    <li><a href="">菜单33</a></li>
-                </ul>
-                -->
-
+                <!-- 电脑显示 (>768px) -->
+                <div class="hidden-xs">
+                    <li><a href="{{ route('home') }}"><span class="glyphicon glyphicon-cog"></span>返回首页</a></li>
+                    <li><a href="{{ route('article.index') }}"><span class="glyphicon glyphicon-cog"></span>文章管理</a></li>
+                </div>
+                <!-- 手机显示(<768px) -->
+                <div class="visible-xs-block">
+                    <li><a href="{{ route('home') }}">首页</a></li>
+                    <li><a href="{{ route('article.index') }}">文章</a></li>
+                </div>
             </ul>
         </div>
         {{--右侧内容区--}}
@@ -75,32 +57,6 @@
             </div>
 
         </div>
-    </div>
-
-    {{--手机版 管理后台--}}
-    <div class="visible-xs-block">
-        {{--左侧垂直菜单栏--}}
-        <div class="z-admin-menu-phone">
-            <div class="header">
-                <a href="{{ route('admin') }}"><span class="glyphicon glyphicon-cog"></span></a>
-            </div>
-            <ul>
-
-                <li><a href="{{ route('home') }}">首页</a></li>
-
-                <li><a href="{{ route('article.index') }}">文章</a></li>
-
-            </ul>
-        </div>
-        {{--右侧内容区--}}
-        <div class="z-admin-box-phone">
-            <div class="content">
-                @include('shared.messages')
-                @yield('content')
-            </div>
-        </div>
-    </div>
-
 </body>
 <!-- Scripts -->
 <script src="/js/app.js"></script>
