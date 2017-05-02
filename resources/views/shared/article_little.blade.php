39fbb45c349249cae9c2ee3f83e1faa2467912d0
SadCreeper <87826632@qq.com>
2017-05-02 15:43:15 +0800

    change the line-height of home page & articles list page

--- resources/views/shared/article_little.blade.php
+++ resources/views/shared/article_little.blade.php
@@ -1,12 +1,14 @@
 <div class="z-article-little">
-    <div class="row">
+    <div class="row" style="display:flex;align-items:center">
         <div class="col-md-8 col-sm-8">
             <a href="{{ route('article.show', $article->id) }}"><h5>{{ $article->title }}</h5></a>
         </div>
         <div class="col-md-4 col-sm-4">
-            <span class="glyphicon glyphicon-eye-open"></span><span style="margin-right:10px"> {{ $article->view }}</span>
-            <span class="glyphicon glyphicon-edit"></span><span style="margin-right:10px"> {{ $article->comment }}</span>
-            <span class="glyphicon glyphicon-time"></span><span> {{ $article->created_at->diffForHumans() }}</span>
+            <div class="">
+                <span class="glyphicon glyphicon-eye-open"></span><span style="margin-right:10px"> {{ $article->view }}</span>
+                <span class="glyphicon glyphicon-edit"></span><span style="margin-right:10px"> {{ $article->comment }}</span>
+                <span class="glyphicon glyphicon-time"></span><span> {{ $article->created_at->diffForHumans() }}</span>
+            </div>
         </div>
     </div>
 </div>
