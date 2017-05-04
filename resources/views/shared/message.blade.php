2d8e2cc037cc958ce96e9e0137ca4f5165db9b1d
SadCreeper <87826632@qq.com>
2017-05-04 18:42:56 +0800

    change message name of anonymouts user

--- resources/views/shared/message.blade.php
+++ resources/views/shared/message.blade.php
@@ -1,7 +1,7 @@
 <!-- 留言 -->
 <div class="z-comment">
     @if( $message->user_id == 0)
-        <p><b>一位不愿透露姓名的用户</b><span style="float:right">{{ $message->id  }} 楼</span></p>
+        <p><b>匿名</b><span style="float:right">{{ $message->id  }} 楼</span></p>
     @else
         <p><b>{{ $message->user->name }}</b><span style="float:right">{{ $message->id  }} 楼</span></p>
     @endif
