fba6d10fc7cca903a08240fe3d6cf53a4e0ab276
SadCreeper <87826632@qq.com>
2017-06-02 15:17:56 +0800

    add ads

--- resources/views/shared/ads.blade.php
+++ resources/views/shared/ads.blade.php
@@ -4,6 +4,33 @@
         广告位
     </div>
     <div class="z-panel-body">
+        <div id="carousel-example-generic-2" class="carousel slide" data-ride="carousel" style="margin-bottom: 20px;">
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                    <a href="{{ route('article.show', 15) }}"><img src="http://onnx37np8.bkt.clouddn.com/17-6-2/32623317.jpg" alt="imax1"></a>
+                    <div style="position: relative;height:30px;top:-30px;background-color:black;color:white;font-size:15px;filter:alpha(opacity=60);-moz-opacity:0.6;-khtml-opacity: 0.6;opacity: 0.6;">
+                        【樱桃】现摘现发，全国包邮
+                    </div>
+                </div>
+                <div class="item">
+                    <a href="{{ route('article.show', 15) }}"><img src="http://onnx37np8.bkt.clouddn.com/17-6-2/32623317.jpg" alt="imax1"></a>
+                    <div style="position: relative;height:30px;top:-30px;background-color:black;color:white;font-size:15px;filter:alpha(opacity=60);-moz-opacity:0.6;-khtml-opacity: 0.6;opacity: 0.6;">
+                        【樱桃】现摘现发，全国包邮
+                    </div>
+                </div>
+            </div>
+
+            <!-- Controls -->
+            <div class="z-imax">
+                <a class="left" href="#carousel-example-generic-2" data-slide="prev">
+                    <span class="button glyphicon glyphicon-chevron-left"></span>
+                </a>
+                <a class="right" href="#carousel-example-generic-2" data-slide="next">
+                    <span class="button glyphicon glyphicon-chevron-right"></span>
+                </a>
+            </div>
+        </div>
         <ul>
             <li><a href="#author_info">广告位招商-免费添加-限5位</a></li>
         </ul>
