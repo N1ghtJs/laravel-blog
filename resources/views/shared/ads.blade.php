4494ac9dc0695b3acc018402332dc66b28cf95f7
SadCreeper <87826632@qq.com>
2017-07-16 11:21:57 +0800

    add ad

--- resources/views/shared/ads.blade.php
+++ resources/views/shared/ads.blade.php
@@ -8,15 +8,15 @@
             <!-- Wrapper for slides -->
             <div class="carousel-inner" role="listbox">
                 <div class="item active">
-                    <a href="{{ route('article.show', 15) }}"><img src="http://onnx37np8.bkt.clouddn.com/17-6-2/32623317.jpg" alt="imax1"></a>
+                    <a href="{{ route('article.show', 15) }}"><img src="http://onnx37np8.bkt.clouddn.com/17-7-16/80718953.jpg" alt="imax1"></a>
                     <div style="position: relative;height:30px;top:-30px;background-color:black;color:white;font-size:15px;filter:alpha(opacity=60);-moz-opacity:0.6;-khtml-opacity: 0.6;opacity: 0.6;">
                         【樱桃】现摘现发，全国包邮
                     </div>
                 </div>
                 <div class="item">
-                    <a href="{{ route('article.show', 15) }}"><img src="http://onnx37np8.bkt.clouddn.com/17-6-2/32623317.jpg" alt="imax1"></a>
+                    <a target="_blank" href="https://m.aliyun.com/act/ambassador/sharetouserh5?userCode=i4iyxzmh&utm_source=i4iyxzmh&code=061CDFW82JtWvO0V4XY82kdxW82CDFW6&state=1&from=groupmessage"><img src="http://onnx37np8.bkt.clouddn.com/17-7-16/13852970.jpg" alt="imax1"></a>
                     <div style="position: relative;height:30px;top:-30px;background-color:black;color:white;font-size:15px;filter:alpha(opacity=60);-moz-opacity:0.6;-khtml-opacity: 0.6;opacity: 0.6;">
-                        【樱桃】现摘现发，全国包邮
+                        【阿里云】点击领取优惠券
                     </div>
                 </div>
             </div>
